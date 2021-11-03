7ce0f29235022b114d07035ad626484ca9ef06e3
unknown <2stndard@gmail.com>
2021-11-03 23:10:32 +0900

    211103-2310

--- bkground/bkground.R
+++ bkground/bkground.R
@@ -38,12 +38,60 @@ df_adj |>
   theme(panel.background = element_blank(), 
         panel.grid = element_line(color = 'grey'))
   
+df_adj |>
+  group_by(province) |>
+  summarise(n = sum(teach_total)) -> df_labelname
+
+## named vector
+
+nv_label_name <- setNames(paste0(unique(df_labelname$province), ':', unique(df_labelname$n)), unique(df_labelname$province))
 
 df_adj |>
   ggplot(aes(x = year, y = stu_total)) +
   geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = teach_total), alpha = 0.05) +
   geom_line(aes(color = sch_class, group = sch_class), size = 1) + 
-  facet_wrap(~province, ncol = 3) +
+  facet_wrap(~province, ncol = 3, labeller = as_labeller(nv_label_name)) +
+  labs(fill = '전체학교수', color = '학교급') + 
+  theme(panel.background = element_blank(), 
+        panel.grid = element_line(color = 'grey')) +
+  scale_fill_gradient(low = "lightblue", high = "darkblue")
+
+
+##  function
+
+fn_label_name <- function(x) paste0(x, ' : 교원수 ', unique(n), '명')
+
+
+df_adj |>
+  ggplot(aes(x = year, y = stu_total)) +
+  geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = teach_total), alpha = 0.05) +
+  geom_line(aes(color = sch_class, group = sch_class), size = 1) + 
+  facet_wrap(~province, ncol = 3, labeller = as_labeller(fn_label_name)) +
+  labs(fill = '전체학교수', color = '학교급') + 
+  theme(panel.background = element_blank(), 
+        panel.grid = element_line(color = 'grey')) +
+  scale_fill_gradient(low = "lightblue", high = "darkblue")
+
+
+## inline function
+
+df_adj |>
+  ggplot(aes(x = year, y = stu_total)) +
+  geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = teach_total), alpha = 0.05) +
+  geom_line(aes(color = sch_class, group = sch_class), size = 1) + 
+  facet_wrap(~province, ncol = 3, labeller = as_labeller(
+    function(x) paste0(x, ' : 교원수 ', unique(label_name$n), '명')
+  )) +
+  labs(fill = '전체학교수', color = '학교급') + 
+  theme(panel.background = element_blank(), 
+        panel.grid = element_line(color = 'grey')) +
+  scale_fill_gradient(low = "lightblue", high = "darkblue")
+
+df_adj |>
+  ggplot(aes(x = year, y = stu_total)) +
+  geom_rect(aes(xmin=-Inf,xmax=Inf,ymin=-Inf,ymax=Inf, fill = teach_total), alpha = 0.05) +
+  geom_line(aes(color = sch_class, group = sch_class), size = 1) + 
+  facet_wrap(~province, ncol = 3, labeller = label_context) +
   labs(fill = '전체학교수', color = '학교급') + 
   theme(panel.background = element_blank(), 
         panel.grid = element_line(color = 'grey')) +
