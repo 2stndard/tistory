5a770f91b90a2ade0bdf4a14805025f76897d6ea
이기준 <2stndard@gmail.com>
2022-06-23 17:34:13 +0900

    220623-1734

--- 명지.R
+++ 명지.R
@@ -1,42 +1,44 @@
 library(MASS)
 library(tidyverse)
+library(readxl)
 
-x <- read.csv('clipboard', sep = '\t')
+if (!require('DescTools')) {
+  install.packages('DescTools')
+  library(DescTools)
+}
 
-data <- x |> group_by(실험군) |>
-  summarise_at(vars(starts_with('X')), mean) |>
+if (!require('robnptests')) {
+  install.packages('robnptests')
+  library(robnptests)
+}
+
+### 데이터 import
+x <- read_excel('d:/R/data/2원본수집데이터 수술실미세먼지농도_210805.xlsx', sheet = 'rawData',
+                ## 첫번째 행은 열 이름으로 설정
+                col_names = TRUE, 
+                ## 열의 타입을 설정, 처음 9개는 문자형으로 다음 79개는 수치형으로 설정
+                col_types = c(rep('text', 5), rep('numeric', 85), 'text'
+                              )
+                )
+
+### 데이터 전처리
+x <- x[, -c(1, 87:91)]
+
+
+data <- x |> filter(PM == '1.0', Location == 'Operator') |>
+  group_by(실험군) |>
+  summarise_at(vars(5:84), mean) |>
   pivot_longer(-1, names_to = '회차', values_to = '측정값') |>
   pivot_wider(names_from = 실험군, values_from = 측정값) |>
   setNames(c('회차', 'before', 'after'))
   
+## 핫지스 레만 측정
+HodgesLehmann(data$before, conf.level = 0.95)
+HodgesLehmann(data$after, conf.level = 0.95)
 
-shapiro.test(data$after)
+## 윌콕스 검증
+wilcox.test(data$before, data$after, conf.int = TRUE, conf.level = 0.95,  paired = TRUE)
 
+hl2_test(data$before, data$after, alternative = "two.sided", method = "asymptotic")
 
-wilcox.test(data$before, data$after, conf.int = TRUE, conf.level = 0.95, paired = TRUE)
-
-
-t.test(data$before, data$after, conf.int = FALSE, conf.level = 0.95, paired = TRUE)
-
-library(DescTools)
-HodgesLehmann(data$before, data$after, conf.level = 0.95)
-
-
-colname <- colnames(x)
-
-time <- as.numeric(gsub('X', '', colname))[-c(1:4)]
-
-car::dwt(car::durbinWatsonTest(lm(data$before ~ time)))
-
-car::dwt(lm(data$before ~ time))
-
-library(forecast)
-
-x1 <- residuals(auto.arima(data$before))
-x2 <- residuals(auto.arima(data$after))
-
-shapiro.test(x1)
-shapiro.test(x2)
-qqnorm(x1)
-
-wilcox.test(x1, x2)
+hodges_lehmann_2sample(data$before, data$after)
