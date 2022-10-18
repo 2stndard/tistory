659ef3d8849d25ca323ae2a9cb9e70df578dcc1b
unknown <you@example.com>
2022-10-18 23:18:39 +0900

    221018-2318

--- ggplot2/labeller/labeller.R
+++ ggplot2/labeller/labeller.R
@@ -24,3 +24,12 @@ n_label <- function(string) {
 df_high |> 
   filter(학위과정 == '대학과정') |>
   summarise(n = n()) |> pull()
+
+
+df_high |> 
+  filter(정원대비입학생 < 3) |> 
+  ggplot(aes(x = 대계열, y = 정원대비입학생)) +
+  geom_boxplot() +
+  geom_hline(aes(yintercept = 1), col = 'red') +
+  facet_wrap(~학위과정, labeller = labeller(학위과정 = label_wrap_gen(50))) +
+  theme(axis.text.x = element_text(angle= 90))
